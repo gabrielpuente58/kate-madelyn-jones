153989e2f482997469275890c1574a091c86c1b9
Gabriel Puente <gabrieldp04@icloud.com>
2025-06-21 20:49:53 -0600

    Not done but good base to work from

---
+++ script.js
@@ -0,0 +1,15 @@
+const correctPassword = "love2025";
+const modal = document.getElementById("passwordModal");
+const scrapbook = document.getElementById("scrapbook");
+const music = document.getElementById("background-music");
+
+function unlockScrapbook() {
+  const input = document.getElementById("passwordInput").value;
+  if (input === correctPassword) {
+    modal.style.display = "none";
+    scrapbook.style.display = "block";
+    music.play();
+  } else {
+    alert("Incorrect passcode. Please try again.");
+  }
+}
